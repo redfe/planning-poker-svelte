5e85dc7831eb4a39601d923d0b55c6dd05249267
redfe <takayamatetsuya@takayamatetsuyanoMacBook-Pro.local>
2020-11-01 20:47:20 +0900

    コピーボタンを作成した。

--- src/Table.svelte
+++ src/Table.svelte
@@ -11,6 +11,31 @@
       tableState.close();
     }
   }
+
+  function handleCopyButtonClick(event) {
+    const text = createCopyText();
+    navigator.clipboard.writeText(text);
+  }
+
+  function createCopyText() {
+    if ($estimates.length === 0) {
+      return "empty.";
+    }
+    const now = new Date();
+    return (
+      "[" +
+      now.toLocaleDateString() +
+      " " +
+      now.toLocaleTimeString() +
+      "] " +
+      $estimates
+        .sort((e1, e2) => (e1.name > e2.name ? 1 : -1))
+        .reduce(
+          (acc, cur) => (acc ? acc + " " : "") + `${cur.name}(${cur.point})`,
+          null
+        )
+    );
+  }
 </script>
 
 <style>
@@ -21,7 +46,7 @@
       rgba(111, 162, 127, 0.6274509803921569)
     );
     padding: 1rem 1rem 0 0;
-    border-radius: 0.5rem;
+    border-radius: 20px;
     height: 20rem;
     position: relative;
     box-shadow: 0 4px 3px 3px rgba(0, 0, 0, 0.15);
@@ -64,6 +89,27 @@
     transform: translateY(2px);
     box-shadow: 0 2px 0 rgba(0, 0, 0, 0.15);
   }
+  .copy-button {
+    position: absolute;
+    display: inline-block;
+    top: 0;
+    right: 0;
+    background-color: silver;
+    border-radius: 0 1.2rem;
+    font-size: 0.5rem;
+    padding: 0.25rem;
+    cursor: pointer;
+    text-align: center;
+    width: 2rem;
+    box-shadow: -1px 3px 3px 0 rgba(0, 0, 0, 0.15);
+    -webkit-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+  .copy-button:active {
+    transform: translateY(2px) translateX(-2px);
+    box-shadow: 0 2px 0 rgba(0, 0, 0, 0.15);
+  }
 </style>
 
 <div class="table">
@@ -79,5 +125,8 @@
     <div class="open-button" on:click={handleOpenButtonClick}>
       {$tableState.closed ? 'open' : 'return'}
     </div>
+  {/if}
+  {#if !$tableState.closed}
+    <div class="copy-button" on:click={handleCopyButtonClick}>Copy</div>
   {/if}
 </div>
