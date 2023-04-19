e6b1a4f8c57b3f454013cae5fb74ecdfe4190afe
redfe <tetsu999@gmail.com>
2023-04-19 22:34:19 +0900

    コンポーネントのストーリーを削除

--- my-app/src/lib/components/Card.stories.ts
+++ my-app/src/lib/components/Card.stories.ts
@@ -4,7 +4,6 @@ import Card from './Card.svelte';
 
 // More on how to set up stories at: https://storybook.js.org/docs/svelte/writing-stories/introduction
 const meta = {
-	title: 'Components/Card',
 	component: Card,
 	tags: ['autodocs'],
 	args: {
