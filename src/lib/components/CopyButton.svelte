f1f1c73b16048106965ca405877ca601c9136d32
redfe <tetsu999@gmail.com>
2023-04-22 14:33:15 +0900

    フォーマット実施

--- src/lib/components/CopyButton.svelte
+++ src/lib/components/CopyButton.svelte
@@ -1,49 +1,50 @@
 <script lang="ts">
-  export let isCopied = false;
+	export let isCopied = false;
 </script>
 
 <button
-  class={['copy-button', isCopied ? 'copied' : ''].join(' ')}
-  on:click
-  disabled={isCopied ? true : false}>{isCopied ? 'COPIED' : 'COPY'}</button>
+	class={['copy-button', isCopied ? 'copied' : ''].join(' ')}
+	on:click
+	disabled={isCopied ? true : false}>{isCopied ? 'COPIED' : 'COPY'}</button
+>
 
 <style>
-  .copy-button {
-    position: absolute;
-    display: inline-block;
-    border-radius: 0 1.2rem;
-    font-size: 0.5rem;
-    padding: 0.25rem;
-    width: 4rem;
-    box-shadow: -1px 3px 3px 0 rgba(0, 0, 0, 0.15);
-    transition: color 1s, box-shadow 0 1s;
-  }
-  .copy-button.copied {
-    color: rgb(161, 18, 18);
-    box-shadow: none;
-    font-size: 0.2rem;
-    font-weight: bold;
-    cursor: auto;
-    transform: translateY(2px);
-    box-shadow: 0 2px 0 rgba(0, 0, 0, 0.15);
-  }
+	.copy-button {
+		position: absolute;
+		display: inline-block;
+		border-radius: 0 1.2rem;
+		font-size: 0.5rem;
+		padding: 0.25rem;
+		width: 4rem;
+		box-shadow: -1px 3px 3px 0 rgba(0, 0, 0, 0.15);
+		transition: color 1s, box-shadow 0 1s;
+	}
+	.copy-button.copied {
+		color: rgb(161, 18, 18);
+		box-shadow: none;
+		font-size: 0.2rem;
+		font-weight: bold;
+		cursor: auto;
+		transform: translateY(2px);
+		box-shadow: 0 2px 0 rgba(0, 0, 0, 0.15);
+	}
 
-  button {
-    text-align: center;
-    border: none;
-    border-radius: 0.5rem;
-    background-color: silver;
-    cursor: pointer;
-    box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.15);
-    transition: transform 0.1s ease-in-out 0s;
-    user-select: none;
-    padding: 0;
-    display: block;
-    outline: none;
-  }
+	button {
+		text-align: center;
+		border: none;
+		border-radius: 0.5rem;
+		background-color: silver;
+		cursor: pointer;
+		box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.15);
+		transition: transform 0.1s ease-in-out 0s;
+		user-select: none;
+		padding: 0;
+		display: block;
+		outline: none;
+	}
 
-  button:active {
-    transform: translateY(2px);
-    box-shadow: 0 2px 0 rgba(0, 0, 0, 0.15);
-  }
+	button:active {
+		transform: translateY(2px);
+		box-shadow: 0 2px 0 rgba(0, 0, 0, 0.15);
+	}
 </style>
--- src/lib/components/UnsplashCredit.svelte
+++ src/lib/components/UnsplashCredit.svelte
@@ -1,33 +1,35 @@
 <div class="unsplash-credit">
-  <a
-    href="https://unsplash.com"
-    target="_blank"
-    rel="noopener noreferrer"
-    title="Photo by Unsplash">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
-      class="_1Jlgk"
-      version="1.1"
-      viewBox="0 0 32 32"
-      aria-labelledby="unsplash-home"
-      aria-hidden="false">
-      <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z" />
-    </svg>
-  </a>
+	<a
+		href="https://unsplash.com"
+		target="_blank"
+		rel="noopener noreferrer"
+		title="Photo by Unsplash"
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="20"
+			height="20"
+			class="_1Jlgk"
+			version="1.1"
+			viewBox="0 0 32 32"
+			aria-labelledby="unsplash-home"
+			aria-hidden="false"
+		>
+			<path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z" />
+		</svg>
+	</a>
 </div>
 
 <style>
-  .unsplash-credit {
-    background-color: rgba(255, 255, 255, 0.5);
-    position: absolute;
-    right: 0.75rem;
-    top: 0.75rem;
-    padding: 0.5rem;
-    padding-bottom: 0.1rem;
-  }
-  .unsplash-credit:hover {
-    background-color: rgba(255, 255, 255, 0.8);
-  }
+	.unsplash-credit {
+		background-color: rgba(255, 255, 255, 0.5);
+		position: absolute;
+		right: 0.75rem;
+		top: 0.75rem;
+		padding: 0.5rem;
+		padding-bottom: 0.1rem;
+	}
+	.unsplash-credit:hover {
+		background-color: rgba(255, 255, 255, 0.8);
+	}
 </style>
