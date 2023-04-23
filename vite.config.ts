2765fcfaea67657c8be5ba82587c9d8bfc0795a6
redfe <tetsu999@gmail.com>
2023-04-23 22:17:05 +0900

    CI失敗対応

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		env: {
-			PUBLIC_BASE_URL: ''
-		},
 		environment: 'jsdom',
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
